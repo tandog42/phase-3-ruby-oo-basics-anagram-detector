fae8678da66121d33555a5f61ef5549b4b98fa4c
Tanner <troxs690@gmail.com>
2022-09-22 12:27:08 -0500

    d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+ attr_accessor :name
+
+  def initialize(word)
+    @name = word
+    
+end
+def match(array)
+  array.select{|q| q.split("").sort == @name.split("").sort}
+
+end
+end
